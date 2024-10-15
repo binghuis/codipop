3a96d32ed3e1834ec7875b06cb0be822eec82d49
宋秉徽 <451000936@qq.com>
2024-10-15 18:32:02 +0800

    style: :lipstick: 代码格式化

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,6 @@
 export default {
     '*.{md,json}': ['prettier --cache --write --no-error-on-unmatched-pattern'],
     '*.{css,less,scss}': ['stylelint --fix', 'prettier --cache --write'],
-    '*.{js,jsx}': ['eslint --fix', 'prettier --cache --write'],
+    '*.{js,jsx,vue}': ['eslint --fix', 'prettier --cache --write'],
     '*.{ts,tsx}': ['eslint --fix', 'prettier --cache --parser=typescript --write']
 }
--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('stylelint').Config} */
 export default {
-  extends: ["stylelint-config-standard"],
-  cache: true,
-  overrides: [
-    {
-      files: ["**/*/*.scss"],
-      extends: ["stylelint-config-recommended-scss"],
-    },
-    {
-      files: ["**/*/*.vue"],
-      extends: ["stylelint-config-recommended-vue"],
-      rules: {},
-    },
-  ],
-};
+    extends: ['stylelint-config-standard', 'stylelint-config-tailwindcss'],
+    cache: true,
+    overrides: [
+        {
+            files: ['**/*/*.scss'],
+            extends: ['stylelint-config-recommended-scss']
+        },
+        {
+            files: ['**/*/*.vue'],
+            extends: ['stylelint-config-recommended-vue'],
+            rules: {}
+        }
+    ]
+}
